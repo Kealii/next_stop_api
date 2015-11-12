f98e5ef5600a36edc2ae978cb1ecc25d12964243
Michael Wong <michael.brian.wong@gmail.com>
2015-11-12 09:36:45 -0700

    changes default format to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json } do
       resources :routes, only: [:show, :index]
     end
   end
