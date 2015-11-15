f5105cfa1ec879689ac57bfd8b40d39d134c4ccc
Michael Wong <michael.brian.wong@gmail.com>
2015-11-15 14:49:38 -0700

    added the code i want to exist, need to make it work!

--- app/controllers/api/v1/routes_controller.rb
+++ app/controllers/api/v1/routes_controller.rb
@@ -6,4 +6,14 @@ class Api::V1::RoutesController < ApplicationController
   def index
     respond_with Route.all
   end
+
+  def stops
+    respond_with Route.find_by(route_id: find_params[:route_id]).stops
+  end
+
+  private
+
+  def find_params
+    params.permit(:route_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :routes,     only: [:show, :index]
+      resources :routes,     only: [:show, :index] do
+        get :stops
+      end
+
       resources :stop_times, only: [:show, :index]
       resources :stops,      only: [:show, :index]
       resources :trips,      only: [:show, :index]
